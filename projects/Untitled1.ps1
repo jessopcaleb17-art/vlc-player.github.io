fa821351c38fdbdb6b0db93e7290150c88a6da50
jessopcaleb17-art <jessopcaleb17@gmail.com>
2025-12-17 18:39:47 -0700

    Create Untitled1.ps1

---
+++ projects/Untitled1.ps1
@@ -0,0 +1,37 @@
+﻿2025-12-17 17:40:15.399 [info] Installing C# dependencies...
+2025-12-17 17:40:15.399 [info] 
+
+2025-12-17 17:40:15.399 [info] Platform: win32, x86_64
+
+2025-12-17 17:40:15.399 [info] 
+
+2025-12-17 17:40:15.400 [info] Downloading package 'Language server for Roslyn Copilot integration' 
+2025-12-17 17:40:15.822 [info] (47 KB)
+2025-12-17 17:40:15.822 [info] .......
+2025-12-17 17:40:15.946 [info] .......
+2025-12-17 17:40:15.948 [info] ......
+2025-12-17 17:40:15.948 [info]  Done!
+
+2025-12-17 17:40:15.949 [info] Validating download...
+
+2025-12-17 17:40:15.949 [info] Integrity Check succeeded.
+
+2025-12-17 17:40:15.949 [info] Installing package 'Language server for Roslyn Copilot integration'
+
+2025-12-17 17:40:15.949 [info] 
+
+2025-12-17 17:40:15.961 [info] Finished
+
+2025-12-17 17:40:15.961 [info] 
+
+2025-12-17 17:40:16.746 [info] Locating .NET runtime version 9.0.1
+2025-12-17 17:40:17.001 [info] Dotnet path: C:\Program Files\dotnet\dotnet.exe
+2025-12-17 17:40:17.002 [info] Activating C# + C# Dev Kit...
+2025-12-17 17:40:20.692 [info] [stdout] info: Program[0]
+      Server started with process ID 25576
+
+2025-12-17 17:40:34.763 [info] [stdout] {"pipeName":"\\\\.\\pipe\\0e92dd59"}
+
+2025-12-17 17:40:34.763 [info] received named pipe information from server
+2025-12-17 17:40:34.764 [info] client has connected to server
+2025-12-17 17:40:34.883 [info] [Program] Language server initialized
